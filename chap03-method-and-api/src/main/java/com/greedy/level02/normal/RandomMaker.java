a4c63c744735bf2f2b912d503c6feaa230cb6f31
Yangha Park <64etuor@gmail.com>
2025-02-18 16:46:17 +0900

    some dummy deleted

--- chap03-method-and-api/src/main/java/com/greedy/level02/normal/RandomMaker.java
+++ chap03-method-and-api/src/main/java/com/greedy/level02/normal/RandomMaker.java
@@ -6,8 +6,10 @@ public class RandomMaker {
 
     }
     public static int randomNumber(int min, int max) {
+
         return (int) (Math.random() * (max - min + 1)) + min;
     }
+
     public static String randomUpperAlphabet(int length) {
         StringBuilder alpha_list = new StringBuilder();
         for (int i = 0; i < length; i++) {
