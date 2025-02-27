e00855593ed8f8fa9a4c089402d5cab905fdd9ae
Yangha Park <64etuor@gmail.com>
2025-02-27 17:37:06 +0900

    feat:chap13-collection all done, avengers assembled

--- chap13-collection/src/main/java/com/ohgiraffers/level01/basic/Application5_tree.java
+++ chap13-collection/src/main/java/com/ohgiraffers/level01/basic/Application5_tree.java
@@ -1,5 +1,8 @@
 package com.ohgiraffers.level01.basic;
 
+import java.util.Scanner;
+import java.util.TreeSet;
+
 /*
 * 단어 입력 ('exit' 입력 시 종료): 안녕
 단어 입력 ('exit' 입력 시 종료): java
@@ -8,15 +11,9 @@ package com.ohgiraffers.level01.basic;
 단어 입력 ('exit' 입력 시 종료): exit
 정렬 된 단어 : [collection, java, 안녕, 프로그래밍]
 * */
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
-
-public class Application5 {
+public class Application5_tree {
     public static void main(String[] args) {
-        List<String> words = new ArrayList<>();
+        TreeSet<String> words = new TreeSet<>();
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -28,8 +25,7 @@ public class Application5 {
             }
             words.add(inputWord);
         }
-        words.sort(Comparator.naturalOrder());
         System.out.println("정렬된 단어 : " + words);
-
     }
+    
 }
